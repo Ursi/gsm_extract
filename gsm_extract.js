284fc242502cb4443d7fe561f0e07915e28ee4e8
Mason Mackaman <masondeanm@aol.com>
2019-06-29 19:58:24 -0400

    better deleting

--- gsm_extract.js
+++ gsm_extract.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const config = 'gsm_extract.json';
 
+const filterFiles = [
+    '.git',
+    'README.md',
+]
+
 function del(path) {
     if (process.argv[2] != 'soft' && fs.existsSync(path)) fs.unlinkSync(path);
 }
@@ -9,7 +14,6 @@ function del(path) {
 (function gsmExtract(dir, extract = false) {
     console.log(dir);
     if (fs.existsSync(path.join(dir, config))) {
-        if (process.argv[2] != 'soft') fs.unlinkSync(path.join(dir, '.gitmodules'));
         try {
             var configObj = JSON.parse(fs.readFileSync(path.join(dir, config)));
         } catch (error) {
@@ -31,12 +35,12 @@ function del(path) {
 
     if (extract) {
         for (let file of fs.readdirSync(dir)) {
-            if (file != '.git') {
+            if (!filterFiles.includes(file)) {
                 if (process.argv[2] == 'soft') {
                     fs.copyFileSync(path.join(dir, file), path.join(dir, '..', file));
                 } else {
                     fs.renameSync(path.join(dir, file), path.join(dir, '..', file));
-                    if (fs.existsSync(path.join(dir, '.git'))) fs.unlinkSync(path.join(dir, '.git'));
+                    for (let file of filterFiles) del(path.join(dir, file));
                     fs.rmdir(dir, console.log);
                 }
             }
